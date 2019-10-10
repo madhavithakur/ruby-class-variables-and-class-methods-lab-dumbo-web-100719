995c7c66ff7d06ea2406af4092d8bb84c86b9e81
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-10 22:44:08 +0000

    song

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song 
+  
+  attr_reader :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.genre_count
+    hash = Hash.new(0)
+    @@genres.each {|ele| hash[ele] += 1}
+    hash
+  end
+  
+  def self.artist_count 
+    hash = Hash.new(0)
+    @@artists.each {|ele| hash[ele] += 1}
+    hash
+  end 
+  
+end 
